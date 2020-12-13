7f79cecca3e046cead8e490492a8ebc4c475e309
Touza2el <59705964+Touza2el@users.noreply.github.com>
2020-12-13 02:32:34 +0100

    First Commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Start Header Section
+
+const mobileNavigation = document.querySelector('.mobile-navigation');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerMenu.addEventListener('click', function () {
+  mobileNavigation.classList.toggle('open');
+});
+
+// End Header Section
